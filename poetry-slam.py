cecd3082e997554407266ba512f80d3bc99809c1
Sawyer Cherry <sawyeracherry@gmail.com>
2020-10-02 00:15:01 -0500

    polished code and comments, all done.

--- poetry-slam.py
+++ poetry-slam.py
@@ -1,5 +1,6 @@
 from random import choice
 
+#get file line strings
 def get_file_lines(filename):
     in_file = open(filename, "r")
     lines = in_file.readlines()
@@ -8,6 +9,7 @@ def get_file_lines(filename):
 
 print(get_file_lines('poem.txt'))
 
+# backward lines
 def lines_printed_backwards(lines_list):
     #this reverses a list
     lines_list.reverse()
@@ -17,13 +19,12 @@ def lines_printed_backwards(lines_list):
         line_num = lines_length - i
         print(f"{line_num} {line}")
 
-
-
+# random lines
 def lines_printed_random(lines_list):
     for lines in lines_list:
         print(choice(lines_list))
 
-
+# custom lines
 def lines_printed_custom(lines_list):
     lines_length = len(lines_list)
     for i in range(len(lines_list)):
@@ -38,4 +39,7 @@ poem_lines = get_file_lines('poem.txt')
 #lines_printed_backwards(lines_list)
 #lines_printed_custom(lines_list)
 #lines_printed_random(poem_lines)
+
+
+# Collab with Lon, Liz, and Arjun
 
